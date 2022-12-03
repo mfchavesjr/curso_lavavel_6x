db69a9b50352e377c0c0e5263eb2c188f2cc90f6
Mauricio Chaves <feira.nova@gmail.com>
2022-12-02 22:00:14 -0300

    aula de rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,88 @@
 |
 */
 
+use Illuminate\Routing\Route;
+
+Route::get('/login', function () {
+    return 'Tela Login';
+})->name('login');
+
+Route::get('/admin/dashboard', function(){
+    return 'Home Admin';
+})->middleware('auth');
+// OU
+Route::middleware(['auth'])->group( function () {
+
+    Route::prefix('admin')->group(function () {
+
+        Route::namespace('Admin')->group( function () {
+
+            Route::name('admin')->group( function () {
+
+                Route::get('/', function(){
+                    return 'Home Admin';
+                });
+
+                Route::get('/financeiro', function(){
+                    return 'Home Financeiro';
+                });
+
+                Route::get('/juridico', function(){
+                    return 'Home Juridico';
+                });
+            });
+        });
+    });
+});
+
+// OU
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin',
+], function () {
+    Route::get('/', function(){
+        return 'Home Admin';
+    });
+
+    Route::get('/financeiro', function(){
+        return 'Home Financeiro';
+    });
+
+    Route::get('/juridico', function(){
+        return 'Home Juridico';
+    });
+});
+
+Route::redirect('/redirect1', '/redirect2');
+// OU
+Route::get('/redirect1', function(){
+    return redirect('/redirect2');
+})->name('url.name');
+
+Route::get('/redirect3', function(){
+    return redirect()->route('url.name');
+});
+
+Route::get('/redirect2', function(){
+    return 'Redirect 2';
+});
+
+Route::get('/categorias/{idPoduto?}', function($idPoduto = '') {
+    return "Produto(s): {$idPoduto}";
+});
+
+Route::get('/categorias/{flag}', function($paramentro) {
+    return "Produtos da categoria: {$paramentro}";
+});
+
+Route::get('/categorias/{paramentro}/posts', function($paramentro) {
+    return "Posts da categoria: {$paramentro}";
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });

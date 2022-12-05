cca0e8efcf40c93605e7aaefb10d177bdd499227
Mauricio Chaves <feira.nova@gmail.com>
2022-12-05 15:30:43 -0300

    aula 2 controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,4 +16,29 @@ class ProductController extends Controller
 
         return $products;
     }
+
+    public function show($id){
+        return "Exibindo o produto de id: {$id}";
+    }
+
+    public function create(){
+        return "Exibindo o form de cadastro de Produtos";
+    }
+
+    public function store(){
+        return "Cadastrando um novo Produto";
+    }
+
+    public function edit($id){
+        return "Form para editar o produto de id: {$id}";
+    }
+
+    public function update($id){
+        return "Editando o Produto: {$id}";
+    }
+
+    public function destroy($id){
+        return "Deletando o Produto: {$id}";
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('/products', 'ProductController@index')->name('products.index');
+Route::get('/products/{id}', 'ProductController@show')->name('products.show');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::post('/products', 'ProductController@store')->name('products.store');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');

830dd218b83a55ed27a5208c7bd6e2b2a2530c5d
Mauricio Chaves <feira.nova@gmail.com>
2022-12-06 09:00:11 -0300

    aula 3 controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,39 +6,79 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-
-        $products = [
-            'Product 01',
-            'Product 02',
-            'Product 03'
-        ];
-
-        return $products;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
 
-    public function show($id){
-        return "Exibindo o produto de id: {$id}";
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function create(){
-        return "Exibindo o form de cadastro de Produtos";
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function store(){
-        return "Cadastrando um novo Produto";
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
     }
 
-    public function edit($id){
-        return "Form para editar o produto de id: {$id}";
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
     }
 
-    public function update($id){
-        return "Editando o Produto: {$id}";
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
     }
 
-    public function destroy($id){
-        return "Deletando o Produto: {$id}";
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', 'ProductController@index')->name('products.index');
-Route::get('/products/{id}', 'ProductController@show')->name('products.show');
-Route::get('/products/create', 'ProductController@create')->name('products.create');
-Route::post('/products', 'ProductController@store')->name('products.store');
-Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
-Route::put('/products/{id}', 'ProductController@update')->name('products.update');
-Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::get('/login', function () {
+    return 'Login';
+});
+
+//Route::resource('products', 'ProductController');
+

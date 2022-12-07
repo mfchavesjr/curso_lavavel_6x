56dd74efecfbf2873204198c37c44e3e4619a6e0
Mauricio Chaves <feira.nova@gmail.com>
2022-12-07 15:32:21 -0300

    Aula Views 27

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -3,6 +3,18 @@
 @section('title', 'Gestão de Produtos')
 
 @section('content')
+
+    @component('admin.components.card')
+        @slot('title')
+            <h1>Título Card</h1>
+        @endslot
+        <p>Um card de exemplo</p>
+    @endcomponent
+
+    <hr>
+    @include('admin.includes.alerts', ['content' => 'Alerta de preços de produtos'])
+    <hr>
+
     <h1>Exibindo os Produtos</h1>
 
     {{ $teste }}
